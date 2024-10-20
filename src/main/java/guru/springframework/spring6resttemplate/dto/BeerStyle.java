542ceae39269a1b50e28220695c35a48132cdb6b
dboeckli <dboeckli@gmail.com>
2024-10-20 10:46:25 +0200

    initial setup

---
+++ src/main/java/guru/springframework/spring6resttemplate/dto/BeerStyle.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring6resttemplate.dto;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
