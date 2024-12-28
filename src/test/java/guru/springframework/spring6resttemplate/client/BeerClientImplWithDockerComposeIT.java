4e00880db5f399d0a1cee09b3096aacd65dd43b4
Dominique Boeckli <dboeckli@gmail.com>
2024-12-28 17:09:33 +0100

    add tests with testcontainer

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplWithDockerComposeIT.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplWithDockerComposeIT.java
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 // Remark: Currently this test only works when the project spring-6-rest-mvc is running listening on port 80. 
 // Remark: Therefore the test will fail in github actions. We should mock the rest template
 //@Disabled("these tests are only running when the mvc-server and authentication-server have been started")
-class BeerClientImplIT {
+class BeerClientImplWithDockerComposeIT {
     
     @Autowired
     private BeerClientImpl beerClient;
