c7827b7110c69d5f1c05ab1cd39827b119dc351c
adm-boeckli <dboeckli@gmail.com>
2025-05-16 11:29:44 +0200

    setup some basics. fixup 3

--- src/main/java/guru/springframework/spring6resttemplate/config/LogbookConfig.java
+++ src/main/java/guru/springframework/spring6resttemplate/config/LogbookConfig.java
@@ -2,7 +2,6 @@ package guru.springframework.spring6resttemplate.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.zalando.logbook.HttpLogFormatter;
 import org.zalando.logbook.Sink;
 import org.zalando.logbook.json.JsonHttpLogFormatter;
 import org.zalando.logbook.logstash.LogstashLogbackSink;
@@ -11,10 +10,8 @@ import org.zalando.logbook.logstash.LogstashLogbackSink;
 public class LogbookConfig {
 
     @Bean
-    public Sink LogbookLogStash(){
-        HttpLogFormatter formatter = new JsonHttpLogFormatter();
-        LogstashLogbackSink sink = new LogstashLogbackSink(formatter);
-        return sink;
+    public Sink logbookLogStash(){
+        return new LogstashLogbackSink(new JsonHttpLogFormatter());
     }
     
 }
