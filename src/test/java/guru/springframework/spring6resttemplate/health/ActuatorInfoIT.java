e07d7f57290200618899fae61c2441f0a8278be1
Dominique Boeckli <dboeckli@gmail.com>
2026-01-04 11:27:35 +0100

    small fix (#103)

--- src/test/java/guru/springframework/spring6resttemplate/health/ActuatorInfoIT.java
+++ src/test/java/guru/springframework/spring6resttemplate/health/ActuatorInfoIT.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring6resttemplate.core;
+package guru.springframework.spring6resttemplate.health;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
