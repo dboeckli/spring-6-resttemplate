126b63f03c19570ba8216848e90100a75c85c5d1
Dominique Boeckli <dboeckli@gmail.com>
2026-03-15 13:35:09 +0100

    fix jdk and maven update (#115)

--- src/test/java/guru/springframework/spring6resttemplate/health/ActuatorInfoIT.java
+++ src/test/java/guru/springframework/spring6resttemplate/health/ActuatorInfoIT.java
@@ -12,7 +12,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
 
-import static org.hamcrest.Matchers.startsWith;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -40,9 +39,7 @@ class ActuatorInfoIT {
             .andExpect(jsonPath("$.git.commit.id.abbrev").isString())
 
             .andExpect(jsonPath("$.build.artifact").value(buildProperties.getArtifact()))
-            .andExpect(jsonPath("$.build.group").value(buildProperties.getGroup()))
-
-            .andExpect(jsonPath("$.java.version").value(startsWith("21")));
+            .andExpect(jsonPath("$.build.group").value(buildProperties.getGroup()));
     }
 
     @Test
